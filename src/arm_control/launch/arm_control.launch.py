44986ad60d9e7128a2c93e50dc82a96fd8ca87e4
Patrick Adams <patrick4adams@gmail.com>
2025-11-11 01:10:34 +0000

    added example rviz/launch files to modify

--- src/arm_control/launch/arm_control.launch.py
+++ src/arm_control/launch/arm_control.launch.py
@@ -1,18 +1,54 @@
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+# from moveit_configs_utils import MoveItConfigsBuilder
+
+# def generate_launch_description():
+#     moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_dict()
+
+#     # MTC Demo node
+#     keyboard = Node(
+#         package="arm_control",
+#         executable="arm_control_node",
+#         output="screen",
+#         parameters=[
+#             moveit_config,
+#         ],
+#     )
+# moveit_task_constructor_demo
+#     return LaunchDescription([keyboard])
+
+
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_dict()
 
-    # MTC Demo node
-    keyboard = Node(
-        package="arm_control",
-        executable="arm_control_node",
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("moveit_resources_panda")
+        .robot_description(file_path="config/panda.urdf.xacro")
+        .to_moveit_configs()
+    )
+
+    package = "arm_control"
+    package_shared_path = get_package_share_directory(package)
+    node = Node(
+        package=package,
+        executable=LaunchConfiguration("exe"),
         output="screen",
         parameters=[
-            moveit_config,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
+            moveit_config.planning_pipelines,
+            os.path.join(package_shared_path, "config", "panda_config.yaml"),
         ],
     )
 
-    return LaunchDescription([keyboard])
+    arg = DeclareLaunchArgument(name="exe")
+    return LaunchDescription([arg, node])
