fa6b066cabbd1bb880bc8638a41a42bec30e7715
Patrick Adams <patrick4adams@gmail.com>
2025-11-14 01:38:33 +0000

    fixed else if lol

--- src/arm_control/src/ActionInterpreter.cpp
+++ src/arm_control/src/ActionInterpreter.cpp
@@ -17,31 +17,31 @@ public:
         // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", str);
         if (val == '0') {
             RCLCPP_INFO(this->get_logger(), "Starting action 0");
-        } elif (val == '1') {
+        } else if (val == '1') {
             RCLCPP_INFO(this->get_logger(), "Starting action 1");
-        } elif (val == '2') {
+        } else if (val == '2') {
             RCLCPP_INFO(this->get_logger(), "Starting action 2");
-        } elif (val == '3') {
+        } else if (val == '3') {
             RCLCPP_INFO(this->get_logger(), "Starting action 3");
-        } elif (val == '4') {
+        } else if (val == '4') {
             RCLCPP_INFO(this->get_logger(), "Starting action 4");
-        } elif (val == '5') {
+        } else if (val == '5') {
             RCLCPP_INFO(this->get_logger(), "Starting action 5");
-        } elif (val == '6') {
+        } else if (val == '6') {
             RCLCPP_INFO(this->get_logger(), "Starting action 6");
-        } elif (val == '7') {
+        } else if (val == '7') {
             RCLCPP_INFO(this->get_logger(), "Starting action 7");
-        } elif (val == '8') {
+        } else if (val == '8') {
             RCLCPP_INFO(this->get_logger(), "Starting action 8");
-        } elif (val == '9') {
+        } else if (val == '9') {
             RCLCPP_INFO(this->get_logger(), "Starting action 9");
-        } elif (val == 'L') {
+        } else if (val == 'L') {
             RCLCPP_INFO(this->get_logger(), "Making small adjustment left");
-        } elif (val == 'R') {
+        } else if (val == 'R') {
             RCLCPP_INFO(this->get_logger(), "Making small adjustment right");
-        } elif (val == 'U') {
+        } else if (val == 'U') {
             RCLCPP_INFO(this->get_logger(), "Making small adjustment up");
-        } elif (val == 'D') {
+        } else if (val == 'D') {
             RCLCPP_INFO(this->get_logger(), "Making small adjustment down");
         }
       };
